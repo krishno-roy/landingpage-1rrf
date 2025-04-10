cf85fbc3631dbec3da04933f0b560d53b2de2a8c
krishnoroy <krishnorooy@gmail.com>
2025-04-10 17:24:24 +0600

    Initial commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,41 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 import LogoImg from "../assets/heraderlogo.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <header>
-      <nav className="flex justify-between container mx-auto py-2">
-        {/* logo */}
+    <header className="shadow-md">
+      <nav className="flex justify-between items-center container mx-auto py-3 px-4">
+        {/* Logo */}
         <div>
-          <img src={LogoImg} alt="" />
+          <img src={LogoImg} alt="Logo" className="h-10" />
         </div>
-        {/* desktop menu */}
-        <div className="flex gap-4 justify-end items-center">
-          <div className="text-left">
-            <ul className="flex gap-6">
-              <li>
-                <a href="/">About us</a>
-              </li>
-              <li>
-                <a href="/">Courses</a>
-              </li>
-              <li>
-                <a href="/">Events</a>
-              </li>
-              <li>
-                <a href="/">Blogs</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <button className="bg-[#1B99D4] py-2 px-4 text-white text-xl]">
-              Apply Now
-            </button>
-          </div>
+
+        {/* Hamburger Menu (Mobile) */}
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="text-2xl text-[#1B99D4]">
+            {isOpen ? <FiX /> : <FiMenu />}
+          </button>
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex gap-6 items-center">
+          <ul className="flex gap-6">
+            <li>
+              <a href="/" className="hover:text-[#1B99D4]">
+                About us
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-[#1B99D4]">
+                Courses
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-[#1B99D4]">
+                Events
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-[#1B99D4]">
+                Blogs
+              </a>
+            </li>
+          </ul>
+          <button className="bg-[#1B99D4] py-2 px-4 text-white rounded-lg">
+            Apply Now
+          </button>
         </div>
       </nav>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="lg:hidden bg-white px-6 py-4 space-y-4 shadow-md">
+          <ul className="flex flex-col gap-4">
+            <li>
+              <a href="/" className="block hover:text-[#1B99D4]">
+                About us
+              </a>
+            </li>
+            <li>
+              <a href="/" className="block hover:text-[#1B99D4]">
+                Courses
+              </a>
+            </li>
+            <li>
+              <a href="/" className="block hover:text-[#1B99D4]">
+                Events
+              </a>
+            </li>
+            <li>
+              <a href="/" className="block hover:text-[#1B99D4]">
+                Blogs
+              </a>
+            </li>
+          </ul>
+          <button className="w-full bg-[#1B99D4] py-2 text-white rounded-sm">
+            Apply Now
+          </button>
+        </div>
+      )}
     </header>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
